1a1460f9e4d1ffa894f8d88717d74d310f6b260c
pivotal <pivotal@MacBook-Pro-4.local>
2018-08-01 08:58:05 -0500

    tests

---
+++ src/test/java/hello/GreetingControllerTest.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreetingControllerTest {
+
+    @Test
+    public void myTest() {
+        assertTrue(true);
+    }
+}
